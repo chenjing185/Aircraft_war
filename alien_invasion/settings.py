e20c0c4159473af74ad5338dd58611d8a1e87402
灵光的凤凰 <43719769+chenjing185@users.noreply.github.com>
2019-08-04 16:49:44 +0800

    Update settings.py

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -18,6 +18,8 @@ class Settings():
         self.fleet_drop_speed = 10
         #以什么样的速度加快游戏节奏
         self.speedup_scale = 1.1
+        #外星人点数的提高速度
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
     def initialize_dynamic_settings(self):
@@ -35,3 +37,5 @@ class Settings():
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        
+        self.alien_points = int(self.alien_points * self.score_scale)
