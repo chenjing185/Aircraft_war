73665f46ea8b99f29271482f407c300f0339809c
灵光的凤凰 <43719769+chenjing185@users.noreply.github.com>
2019-08-04 16:17:35 +0800

    Update settings.py

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -28,6 +28,8 @@ class Settings():
 
         #fleet_direction为1表示向右；为-1表示向左
         self.fleet_direction = 1
+        #记分
+        self.alien_points = 50
     def increase_speed(self):
         """提高速度设置"""
         self.ship_speed_factor *= self.speedup_scale
