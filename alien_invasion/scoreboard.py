5b1fb30b0477c0b724be15efba9e7cb5caf42b73
灵光的凤凰 <43719769+chenjing185@users.noreply.github.com>
2019-08-06 20:59:12 +0800

    Update scoreboard.py

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -1,4 +1,6 @@
 import pygame.font
+from pygame.sprite import Group
+from ship import Ship
 
 class Scoreboard():
     """显示得分信息类"""
@@ -15,6 +17,7 @@ class Scoreboard():
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
     def prep_score(self):
         """将得分转换成一幅渲染的图像"""
         rounded_score = int(round(self.stats.score,-1))
@@ -43,8 +46,18 @@ class Scoreboard():
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+    def prep_ships(self):
+        """显示还余下多少艘飞船"""
+        self.ships = Group()
+        for ship_number in range(self.stats.ship_left):
+            ship = Ship(self.ai_settings,self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
     def show_score(self):
         """在屏幕上显示得分"""
         self.screen.blit(self.score_image,self.score_rect)
         self.screen.blit(self.high_score_image,self.high_score_rect)
         self.screen.blit(self.level_image,self.level_rect)
+        #绘制飞船
+        self.ships.draw(self.screen)
